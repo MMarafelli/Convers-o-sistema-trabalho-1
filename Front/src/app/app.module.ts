2959ba91d244c46115341989e2bf65b82b97c0ad
Matheus Marafelli <mmarafelli@outlook.com>
2019-05-19 18:09:45 -0300

    Ajustes cliente

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilmeComponent } from './filme/filme.component';
 import { GamesComponent } from './games/games.component';
 import { LanhouseComponent } from './lanhouse/lanhouse.component';
-import { ClienteComponent } from './cliente/cliente.component';
+import { ClienteFormComponent } from './clientes/cliente-form/cliente-form.component';
+import { ClienteListComponent } from './clientes/cliente-list/cliente-list.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +18,12 @@ import { ClienteComponent } from './cliente/cliente.component';
     FilmeComponent,
     GamesComponent,
     LanhouseComponent,
-    ClienteComponent
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Front/src/app/clientes/cliente-form/cliente-form.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { ClientesService } from '../clientes.service';
+
+@Component({
+  selector: 'app-cliente-form',
+  templateUrl: './cliente-form.component.html',
+  styleUrls: ['./cliente-form.component.sass']
+})
+export class ClienteFormComponent implements OnInit {
+
+  private clienteIndex: number;
+  private isNew: boolean;
+  private cliente: Cliente;
+  private subscription: Subscription;
+
+  constructor(private route: ActivatedRoute, private clienteService: ClientesService) { }
+
+  ngOnInit() {
+    this.novo();
+    this.subscription =   this.route.params.subscribe(
+      (params: any) => {
+        if(params.hasOwnProperty('id')) {
+          this.isNew = false;
+          this.clienteIndex = +params['id'];
+          this.clienteService.get(this.clienteIndex)
+          .subscribe(data => this.cliente = data);
+        } else {
+          this.isNew = true;
+        }
+      }
+    )
+  }
+
+  novo() {
+    this.cliente = new Cliente;
+  }
+
+  salvar() {
+    let result;
+    if(this.isNew) {
+      result = this.clienteService.add(this.cliente);
+    } else {
+      result = this.clienteService.update(this.cliente);
+    }
+    this.novo();
+    result.subscribe(data => alert('sucesso' +data),
+    err => {
+      alert("An error occurred " +err);
+    });
+  }
+
+  excluir() {
+    if(this.cliente.codigo == null) {
+      alert("Selecione algum cliente");
+    } else {
+      if(confirm("Você realmente quer excluir o cliente " + this.cliente.nome + " ?"))
+        this.clienteService.remove(this.cliente.codigo)
+        .subscribe(
+          data => this.novo,
+          err => {
+            alert("Cliente não removido.");
+          });
+    }
+  }
+
+}
